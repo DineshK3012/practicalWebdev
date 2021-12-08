c58d6d58dbbac1c8a63e6998ee4b7671cf1ca0e6
Dinesh kumar <87605928+DineshK3012@users.noreply.github.com>
2021-12-08 20:22:42 +0530

    initial commit

--- practical/records.php
+++ practical/records.php
@@ -15,55 +15,66 @@
 
 
     <style>
-    * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        font-family: 'Poppins', sans-serif;
-    }
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+            font-family: 'Poppins', sans-serif;
+        }
 
-    .container {
-        width: 100%;
-    }
+        .container {
+            width: 100%;
+        }
 
-    .container table {
-        width: 95%;
-        margin: 1rem auto;
-        color: var(--dark);
-        /* background-color: white; */
-        color: white;
-        border: 1px solid white;
-        border-radius: 3px;
-        padding: 1rem 0.5rem;
-    }
+        .container button.addrecord {
+            width: 200px;
+            height: 30px;
+            margin: 1rem auto;
+        }
 
-    .container table th {
-        font-size: 1.2rem;
-        margin: 0.5rem 0;
-    }
+        .container button.addrecord a {
+            text-decoration: none;
+            color: black;
+        }
 
-    .container table tr {
-        font-size: 1rem;
-        margin: 0.5rem 0;
-    }
+        .container table {
+            width: 95%;
+            margin: 1rem auto;
+            color: var(--dark);
+            /* background-color: white; */
+            color: white;
+            border: 1px solid white;
+            border-radius: 3px;
+            padding: 1rem 0.5rem;
+        }
 
-    .container table tr .filetext {
-        display: none;
-    }
+        .container table th {
+            font-size: 1.2rem;
+            margin: 0.5rem 0;
+        }
 
-    .container table tr button {
-        font-size: 0.9rem;
-        font-weight: 400;
-        padding: 0.5rem;
-        margin: 0.5rem 0.3rem;
-        outline: none;
-        border: none;
-        background-color: var(--primary);
-    }
+        .container table tr {
+            font-size: 1rem;
+            margin: 0.5rem 0;
+        }
 
-    .container table tr button:hover {
-        box-shadow: inset 0px 0px 3px rgba(0, 0, 0, 0.6);
-    }
+        .container table tr .filetext {
+            display: none;
+        }
+
+        .container table tr button {
+            font-size: 0.9rem;
+            font-weight: 400;
+            padding: 0.5rem;
+            margin: 0.5rem 0.3rem;
+            outline: none;
+            border: none;
+            background-color: var(--primary);
+        }
+
+        .container table tr button:hover {
+            box-shadow: inset 0px 0px 3px rgba(0, 0, 0, 0.6);
+        }
     </style>
 
 </head>
@@ -73,12 +84,14 @@
 
     <!------------------ notes container------------------>
     <div class="container">
+        <button class="addrecord"><a href="partials/_addrecord.php">Add New Result</a></button>
+
         <?php 
-                require 'partials/_connectdb.php'; 
-                
-                if(isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true && $_SESSION['role'] == 'teacher')
-                {   
-                    echo '<table>
+        require 'partials/_connectdb.php'; 
+        
+        if(isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true && $_SESSION['role'] == 'teacher')
+        {   
+            echo '<table>
                             <tr>
                                 <th>S.No</th>
                                 <th>Roll No.</th>
@@ -131,27 +144,27 @@
     <script src="js/jquery-3.6.0.js"></script>
 
     <script>
-    //script to delete the note form the database
-    function deleteRecord(e) {
-        let rollno = e.parentElement.parentElement.querySelector('.rollno').innerHTML;
-        let dob = e.parentElement.parentElement.querySelector('.dob').innerHTML;
-        console.log(rollno);
-        console.log(dob);
+        //script to delete the note form the database
+        function deleteRecord(e) {
+            let rollno = e.parentElement.parentElement.querySelector('.rollno').innerHTML;
+            let dob = e.parentElement.parentElement.querySelector('.dob').innerHTML;
+            console.log(rollno);
+            console.log(dob);
 
-        $.ajax({
-            url: 'partials/_delete.php',
-            method: 'POST',
-            data: {
-                deleteRequest: true,
-                rollno: rollno,
-                dob: dob,
-            },
-            success: function(data) {
-                // console.log(data);
-                location.reload();
-            }
-        });
-    }
+            $.ajax({
+                url: 'partials/_delete.php',
+                method: 'POST',
+                data: {
+                    deleteRequest: true,
+                    rollno: rollno,
+                    dob: dob,
+                },
+                success: function (data) {
+                    // console.log(data);
+                    location.reload();
+                }
+            });
+        }
     </script>
 
 </body>
